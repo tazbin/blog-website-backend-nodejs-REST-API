b7924f877cb6aae10414fb5d64a2dae8e6d6463a
tazbin <md.tazbinur.rahaman@gmail.com>
2022-10-13 09:28:45 +0600

    db name added to url

--- helpers/mongodb.helper.js
+++ helpers/mongodb.helper.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URL, {
-        dbName: process.env.DB_NAME,
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
